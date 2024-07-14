51112abd4af25ec1fa2e55696deb32e50de203c5
ongaeshi <ongaeshi0621@gmail.com>
2024-07-14 22:56:54 +0900

    feat: P5 無しでも動くように

--- main.rb
+++ main.rb
@@ -1,25 +1,25 @@
 WEBGL = 'webgl'
 
 def setup
-  P5.createCanvas(710, 400, WEBGL)
+  createCanvas(710, 400, WEBGL)
 end
 
 def draw
-  P5.background(100)
+  background(100)
 
-  P5.noStroke()
-  P5.fill(50)
-  P5.push()
-  P5.translate(-275, 175)
-  P5.rotateY(1.25)
-  P5.rotateX(-0.9)
-  P5.box(100)
-  P5.pop()
+  noStroke()
+  fill(50)
+  push()
+  translate(-275, 175)
+  rotateY(1.25)
+  rotateX(-0.9)
+  box(100)
+  pop()
 
-  P5.noFill()
-  P5.stroke(255)
-  P5.push()
-  P5.translate(500, P5.height * 0.35, -200)
-  P5.sphere(300)
-  P5.pop()
+  noFill()
+  stroke(255)
+  push()
+  translate(500, height * 0.35, -200)
+  sphere(300)
+  pop()
 end
--- p5opal.rb
+++ p5opal.rb
@@ -1,21 +1,20 @@
 # backtick_javascript: true
 # https://tkitao.hatenablog.com/entry/2015/12/19/192523
 
-module P5
-  def self.method_missing(name, *args)
-    %x{
-      obj = window[name];
-      if (typeof(obj) == 'function') {
-        return window[name].apply(window, args);
-      } else {
-        return window[name];
-      }
-    }
-  end
 
+def method_missing(name, *args)
   %x{
-    window.setup = function() { Opal.top.$setup(); };
-    window.draw = function() { Opal.top.$draw(); };
+    obj = window[name];
+    if (typeof(obj) == 'function') {
+      return window[name].apply(window, args);
+    } else {
+      return window[name];
+    }
   }
 end
+
+%x{
+  window.setup = function() { Opal.top.$setup(); };
+  window.draw = function() { Opal.top.$draw(); };
+}
 
